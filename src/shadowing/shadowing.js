647d0d09a99c86a2328a74a624391203e35f9ce3
etpotato <mareev.pv@gmail.com>
2022-06-16 22:03:30 +0300

    Add shadowing example

---
+++ src/shadowing/shadowing.js
@@ -0,0 +1,10 @@
+let arrUnSort = [1,4,26,9,2];
+let arrSort = [];
+
+function arraySorted(arrUnSort, arrSort) {
+  // same as: var arrSort =
+  arrSort = arrUnSort.sort();
+}
+
+arraySorted(arrUnSort, arrSort);
+console.log(arrSort);
